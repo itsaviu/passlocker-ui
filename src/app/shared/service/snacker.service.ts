cccf326b21190d541dfc10345ee432cfe9929d8b
Avinash <avinash.umasankaramoorthy@coda.global>
2020-01-28 22:56:20 +0530

    Imported Material  UI and Shared Snack Bar

---
+++ src/app/shared/service/snacker.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+
+@Injectable()
+export class SnackerService {
+
+  constructor(private _snackBar: MatSnackBar) { }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+      verticalPosition: 'top',
+      horizontalPosition: 'end',
+      panelClass: ['red-snackbar']
+    });
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerBoxComponent } from './components/container-box/container-box.component';
-import { ModuleWithProviders } from '@angular/compiler/src/core';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { ContentHolderComponent } from './components/content-holder/content-holder.component';
 import { TooltipDirective } from './directives/tooltip.directive';
+import { SnackerService } from './service/snacker.service';
 
 @NgModule({
   declarations: [ContainerBoxComponent, ContentHolderComponent, TooltipDirective],
   imports: [
     CommonModule,
-    
-  ], exports: [ ContainerBoxComponent, ContentHolderComponent, TooltipDirective ]
+    MatSnackBarModule
+  ], exports: [ ContainerBoxComponent, ContentHolderComponent, TooltipDirective ],
+  providers: [SnackerService]
 })
 export class SharedModule { 
 }
