4905addf120c18dec9ec125de4899b7fba5dc96f
Avinash <avi.u95@gmail.com>
2020-02-16 18:40:41 +0530

    Bread Crumbs and Refactoring the Fetch Folder Call

--- src/app/modules/pages/components/vault-container/vault-container.component.ts
+++ src/app/modules/pages/components/vault-container/vault-container.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ManagerService, VaultList } from 'src/app/service/manager.service';
-import { SnackerWorker } from 'src/app/shared/helper/snacker-worker';
+import { ManagerService, VaultList, VaultDetailResp, Vault, VaultBreadCrumbs } from 'src/app/service/manager.service';
+import { SnackerWorker, STATUS } from 'src/app/shared/helper/snacker-worker';
 import { MatDialog } from '@angular/material';
 import { CreateVaultComponent } from '../create-vault/create-vault.component';
 import { CreatePasswordComponent } from '../create-password/create-password.component';
 import { PwdDialogData } from 'src/app/models/pwd-dialog-data';
+import { VaultContainerWorker } from '../../worker/vault-container-worker';
+import { VaultTreeWorker } from '../../worker/vault-tree-worker';
 
 @Component({
   selector: 'app-vault-container',
@@ -15,13 +17,31 @@ export class VaultContainerComponent implements OnInit {
 
   vaultList: VaultList;
   pwdDaialog: PwdDialogData;
-  constructor(private dialog: MatDialog, private managerService: ManagerService) {
-    this.managerService.checkIfVaultContainerUpdate().subscribe((resp: VaultList) => {
-        this.vaultList = resp;
+  selectedVault: Vault;
+  breadCrumbs: VaultBreadCrumbs[];
+  
+  constructor(private dialog: MatDialog, private managerService: ManagerService, private vaultTreeWroker: VaultTreeWorker, private vaultContaineWorker: VaultContainerWorker, private snackerWorker: SnackerWorker) {
+
+    this.managerService.checkIfVaultFolderSectionUpdate().subscribe((resp) => {
+      this.fetchVaultDetails(resp);
+      this.selectedVault = null;
     });
+
+    this.vaultContaineWorker.updatedSelectedVaultReq().subscribe((resp: Vault) => {
+      this.selectedVault = resp;
+    })
   } 
 
   ngOnInit() {
+  }
+
+  fetchVaultDetails(id) {
+    this.managerService.fetchVaultDetails(id).subscribe((resp: VaultDetailResp) => {
+      this.breadCrumbs = resp.breadCrumbs;
+      this.vaultList = resp.folders;
+    }, (error) => {
+      this.snackerWorker.openSnackBar("Something went wrong !", "X", STATUS.FAIL);
+    });
   }
 
   createVault(folderId) {
@@ -37,5 +57,13 @@ export class VaultContainerComponent implements OnInit {
       width: '550px',
       data: this.pwdDaialog
     })
+  }
+
+  selectedFolder(breadCrumb) {
+    if ( this.breadCrumbs[this.breadCrumbs.length - 1].id != breadCrumb.id) {
+      console.log('selected id' + breadCrumb);
+      this.fetchVaultDetails(breadCrumb.id);
+      this.vaultTreeWroker.updateTreeStruct(breadCrumb.id);
+    }
   }
 }
